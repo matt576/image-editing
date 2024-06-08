3d717ec73251827c6e3c2568a110b67ad8819f58
Matheus Ribeiro de Oliveira <s0073@atcremers60.in.tum.de>
2024-06-08 15:39:59 +0200

    superres script

---
+++ code/superres_func.py
@@ -0,0 +1,32 @@
+import requests
+from PIL import Image
+from io import BytesIO
+from diffusers import LDMSuperResolutionPipeline
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model_id = "CompVis/ldm-super-resolution-4x-openimages"
+
+# load model and scheduler
+pipeline = LDMSuperResolutionPipeline.from_pretrained(model_id)
+pipeline = pipeline.to(device)
+
+# let's download an  image
+# url = "https://user-images.githubusercontent.com/38061659/199705896-b48e17b8-b231-47cd-a270-4ffa5a93fa3e.png"
+# response = requests.get(url)
+# low_res_img = Image.open(BytesIO(response.content)).convert("RGB")
+low_res_img = Image.open("inputs/batman.jpg")
+original_width, original_height = low_res_img.size
+desired_width_scale = original_width / 128
+desired_height_scale = original_height / 128
+low_res_img = low_res_img.resize((128, 128))
+low_res_img.save("ldm_low_res.png")
+
+# run pipeline in inference (sample random noise and denoise)
+upscaled_image = pipeline(low_res_img, num_inference_steps=100, eta=1).images[0]
+final_width = int(upscaled_image.width * desired_width_scale)
+final_height = int(upscaled_image.height * desired_height_scale)
+upscaled_image = upscaled_image.resize((final_width, final_height))
+# save image
+
+upscaled_image.save("ldm_generated_image.png")

5afe13d79f504bb893ee84c3b870b788337aebfa
Matheus Ribeiro de Oliveira <s0073@atcremers61.in.tum.de>
2024-06-14 12:58:36 +0200

    gradio app structure

--- code/afm_gradio.py
+++ code/afm_gradio.py
@@ -4,9 +4,7 @@ from PIL import Image
 import requests
 from transformers import SamModel, SamProcessor
 import numpy as np
-
 import os, sys
-
 import torchvision.transforms.v2.functional
 
 from diffusers.utils import load_image, make_image_grid
@@ -14,45 +12,59 @@ from diffusers import StableDiffusionControlNetInpaintPipeline, ControlNetModel,
 
 # imports from code scripts
 from mask_func import get_mask
+from inpaint_func import inputation, make_inpaint_condition, controlnet, pipe
 
-import gradio as gr
-from PIL import Image
 
-# Assuming get_mask is defined in a separate script (mask_func.py)
-from mask_func import get_mask
+def run_afm_app(task_selector, input_image, mask_image, text_input, coord_input):
+    
+    if task_selector == "SAM Mask Generation": ### mask_func.py
+        input_points = None
+        if coord_input is not None:
+            try:
+                x, y = map(int, coord_input.split(','))
+                input_points = [[[x, y]]]
+            except ValueError:
+                print("Invalid input format for coordinates (expected: x,y)")
+        # image = image["composite"]
+        input_image = input_image.convert("RGB")
+        output = get_mask(input_image, input_points)
+        image_array = np.where(output, 255, 0).astype(np.uint8)
+        pil_image = Image.fromarray(image_array)
+        return pil_image
 
-# Pre-defined input points for mask generation (adjust as needed)
-input_points = [[[350, 500]]]
+    if task_selector == "ControlNet Inpainting": ### inpaint_func.py
+        pass
 
-def generate_mask(image):
-  image = image.convert("RGB")
-  output = get_mask(image, input_points)
-  image_array = np.where(output, 255, 0).astype(np.uint8)
-  pil_image = Image.fromarray(image_array)
-  return pil_image
 
-with gr.Blocks() as demo:
-  with gr.Row():
-    # Image upload for mask generation
-    uploaded_image = gr.Image(label="Input Image", type="pil")
-    masked_image = gr.Image(label="Masked Image", type="pil")
-    generate_button = gr.Button("Generate Mask")
+if __name__ == "__main__":
 
-  # Link button click to generate_mask function
-  generate_button.click(fn=generate_mask, inputs=uploaded_image, outputs=masked_image)
+    block = gr.Blocks()
 
-# Launch the Gradio app
-demo.launch(share=True)
+    with block:
+        with gr.Row():
+            with gr.Column():
 
-# def update(name):
-#     return f"Welcome to Gradio, {name}!"
+                task_selector = gr.Dropdown(["SAM Mask Generation", "ControlNet Inpainting"], value="SAM Mask Generation")
+                input_image = gr.Image(label="Raw Input Image", sources='upload', type="pil", value="inputs/batman.jpg")
+                coord_input = gr.Textbox(label="Pixel Coordinates (x,y)", value="350,500") # for SAM
+                mask_image = gr.Image(label="Input Mask (Optional)", sources='upload', type="pil", value="inputs/batman_mask.jpg")
+                text_input = gr.Textbox(label="Text Prompt", value="") # for inpainting
+                
+            with gr.Column():
+                # gallery = gr.Gallery(
+                # label="Generated images", 
+                # show_label=False, 
+                # elem_id="gallery", 
+                # preview=True, 
+                # object_fit="scale-down"
+                # )
+                output_image = gr.Image(label="Generated Image", type="pil")
+                generate_button = gr.Button("Generate")
 
-# with gr.Blocks() as demo:
-#     gr.Markdown("Start typing below and then click **Run** to see the output.")
-#     with gr.Row():
-#         inp = gr.Textbox(placeholder="What is your name?")
-#         out = gr.Textbox()
-#     btn = gr.Button("Run")
-#     btn.click(fn=update, inputs=inp, outputs=out)
+        generate_button.click(
+                fn = run_afm_app,
+                inputs=[task_selector, input_image, mask_image, text_input, coord_input],
+                outputs = output_image
+        )  
 
-# demo.launch()
+    block.launch(share=True)

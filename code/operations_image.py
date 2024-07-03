d6c4989251afddf96d58a19be3dca0fb01401891
Piotr Nobis <s0075@atcremers61.in.tum.de>
2024-07-03 11:36:05 +0200

    evaluation images

--- code/operations_image.py
+++ code/operations_image.py
@@ -20,11 +20,9 @@ def read_and_resize_image(input_image, new_size):
 
 
 # Expand white areas of an image
-def expand_white_areas(image_path, iterations):
+def expand_white_areas(input_image, iterations):
     
-    img = Image.open(image_path).convert('L')
-    img_array = np.array(img)
-    # print("iterations: ", iterations)
+    img_array = np.array(input_image)
 
     # Create a binary mask where white pixels are True and black pixels are False and expand white areas
     binary_mask = img_array == 255
@@ -48,25 +46,10 @@ def shrink_white_areas(mask_image, iterations=1):
 
 if __name__ == "__main__":
 
-    # thresholding black-white
-    input_image_path = 'image-editing/code/inputs/eval/macviej2_mask001.png'
-    output_image_path = 'image-editing/code/inputs/eval/macviej3_mask001.png'
-    thresholded_img = rgb_to_thresholded_grayscale(input_image_path)
-    thresholded_img.save(output_image_path)
-
     # resize image
-    input_path = 'image-editing/code/inputs/eval/macviej2_mask001.png'    # Path to the input image
-    output_path = 'image-editing/code/inputs/eval/macviej3_mask001.png' # Path to save the resized image
-    new_size = (480, 640)                    # New size as a tuple (width, height)
+    input_path = 'image-editing/code/inputs/bear.png'    # Path to the input image
+    output_path = 'image-editing/code/inputs/restyling/bear.png' # Path to save the resized image
+    new_size = (640, 480)                    # New size as a tuple (width, height)
     input_img = Image.open(input_path).convert("RGB")
     output_img = read_and_resize_image(input_img, new_size)
     output_img.save(output_path)
-    
-
-    # dilation of masks
-    dil_iterations = 10
-    image_path = 'inputs/eval/eval_2_mask.png'
-    image_input = Image.open(image_path)
-    image_output = expand_white_areas(image_path, iterations=dil_iterations)
-    output_path = f'inputs/eval/eval_2_mask_d{dil_iterations}.png'
-    image_output.save(output_path)

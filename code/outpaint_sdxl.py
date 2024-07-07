1694458159188ac7d075be2f660049b3ca3c3b62
Piotr Nobis <s0075@node4.slurm.vision.in.tum.de>
2024-07-07 17:32:55 +0200

    best performing sdxl outpainting

--- code/outpaint_sdxl.py
+++ code/outpaint_sdxl.py
@@ -19,11 +19,14 @@ def outpaint_stablediffusionxl(input_image: Image, prompt: str, coordinates: lis
 
     # new image with extended blank spaces
     extended_image = input_image.resize((new_width, new_height))  # the same image resized to new size
-    random_noise_array = np.random.randint(0, 256, (new_height, new_width, 3), dtype=np.uint8)
-    extended_image = Image.fromarray(random_noise_array)
+    #random_noise_array = np.random.randint(0, 256, (new_height, new_width, 3), dtype=np.uint8)
+    #extended_image = Image.fromarray(random_noise_array)
     #white_image = Image.new('RGB', extended_image.size, color='white')
-    #extended_image = Image.new('RGB', (new_width, new_height), color='white')
-    # extended_image = extended_image.filter(ImageFilter.BoxBlur(100))
+    #average_color = np.mean(np.array(input_image), axis=(0, 1))
+    #average_color = tuple(np.round(average_color).astype(int))
+    #print(average_color)
+    #extended_image = Image.new('RGB', (new_width, new_height), color=average_color)
+    #extended_image = extended_image.filter(ImageFilter.BoxBlur(10))
     extended_image.paste(input_image, (coordinates[0], coordinates[2]))
     extended_image.save("outputs/outpainting/extended_image.png")
 
@@ -37,9 +40,6 @@ def outpaint_stablediffusionxl(input_image: Image, prompt: str, coordinates: lis
     # extended_image = extended_image.resize((512, 512))
     # extended_mask = extended_mask.resize((512, 512))
 
-    print(extended_mask.size, extended_mask.info)
-    print(extended_image.size, extended_image.info)
-
     generator = torch.Generator(device="cuda").manual_seed(0)
     output_image = pipe(
         prompt=prompt,
@@ -47,7 +47,7 @@ def outpaint_stablediffusionxl(input_image: Image, prompt: str, coordinates: lis
         mask_image=extended_mask,
         guidance_scale=7.5,
         num_inference_steps=steps,
-        #strength=0.99,
+        strength=0.99,
         # generator=generator
     ).images[0]
 
@@ -59,7 +59,7 @@ if __name__ == "__main__":
 
     
     image = Image.open(f"inputs/outpainting/armchair.png")
-    prompt = "green plants in a pot"
+    prompt = "green plants in a red pot"
     extend_right, extend_left, extend_up, extend_down = 0, 200, 0, 0
     coordinates = [extend_right, extend_left, extend_up, extend_down]
     output_image = outpaint_stablediffusionxl(image, prompt, coordinates, 15)

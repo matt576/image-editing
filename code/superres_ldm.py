7f2dc968683d587a4d21fa66482fb02110525e18
Matheus Ribeiro de Oliveira <s0073@atcremers65.in.tum.de>
2024-07-16 17:28:17 +0200

    minor corrections

--- code/superres_ldm.py
+++ code/superres_ldm.py
@@ -28,6 +28,37 @@ def reassemble_image(patches, img_width, img_height, patch_size):
             k += 1
     return upscaled_img
 
+def superres_gradio(input_image, steps):
+    steps = int(steps)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model_id = "CompVis/ldm-super-resolution-4x-openimages"
+    pipeline = LDMSuperResolutionPipeline.from_pretrained(model_id)
+    pipeline = pipeline.to(device)
+
+    low_res_img = input_image.convert("RGB")
+    original_width, original_height = low_res_img.size
+
+    closest_width = int(round(original_width / 128)) * 128 ###
+    closest_height = int(round(original_height / 128)) * 128 ###
+    low_res_img = low_res_img.resize((closest_width, closest_height))###
+    new_size = (closest_width, closest_height) ###
+
+    patch_size = 128
+    patches = divide_image(low_res_img, patch_size)
+
+    upscaled_patches = []
+    for patch in patches:
+        upscaled_patch = pipeline(image=patch, num_inference_steps=steps, eta=1).images[0]
+        upscaled_patches.append(upscaled_patch)
+
+    upscaled_image = reassemble_image(upscaled_patches, new_size[0], new_size[1], patch_size)
+    upscaled_image = upscaled_image.resize((original_width * 4, original_height * 4))
+
+    os.makedirs("outputs/gradio/superres", exist_ok=True)
+    upscaled_image.save("outputs/gradio/superres/superres_ldm_output_gradio.png")
+    return upscaled_image
+
 if __name__ == "__main__":
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -65,52 +96,6 @@ if __name__ == "__main__":
 
     os.makedirs("outputs/superres", exist_ok=True)
     upscaled_image.save("outputs/superres/superres_ldm_output.png")
-
-def superres_gradio(input_image, steps):
-    steps = int(steps)
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model_id = "CompVis/ldm-super-resolution-4x-openimages"
-    pipeline = LDMSuperResolutionPipeline.from_pretrained(model_id)
-    pipeline = pipeline.to(device)
-
-    low_res_img = input_image.convert("RGB")
-    original_width, original_height = low_res_img.size
-
-    closest_width = int(round(original_width / 128)) * 128 ###
-    closest_height = int(round(original_height / 128)) * 128 ###
-    low_res_img = low_res_img.resize((closest_width, closest_height))###
-    new_size = (closest_width, closest_height) ###
-
-    # if original_width < 350 or original_height < 350:
-    #     low_res_img = low_res_img.resize((256, 256))
-    #     new_size = (256, 256)
-    # else:
-    #     if original_width > original_height:
-    #         low_res_img = low_res_img.resize((768, 512))
-    #         new_size = (768, 512)
-    #     elif original_width < original_height:
-    #         low_res_img = low_res_img.resize((512, 768))
-    #         new_size = (512, 768)
-    #     else:
-    #         low_res_img = low_res_img.resize((512, 512))
-    #         new_size = (512, 512)
-
-    
-    patch_size = 128
-    patches = divide_image(low_res_img, patch_size)
-
-    upscaled_patches = []
-    for patch in patches:
-        upscaled_patch = pipeline(image=patch, num_inference_steps=steps, eta=1).images[0]
-        upscaled_patches.append(upscaled_patch)
-
-    upscaled_image = reassemble_image(upscaled_patches, new_size[0], new_size[1], patch_size)
-    upscaled_image = upscaled_image.resize((original_width * 4, original_height * 4))
-
-    os.makedirs("outputs/gradio/superres", exist_ok=True)
-    upscaled_image.save("outputs/gradio/superres/superres_ldm_output_gradio.png")
-    return upscaled_image
 
 
 """
--- code/superres_upscaler.py
+++ code/superres_upscaler.py
@@ -28,7 +28,38 @@ def reassemble_image(patches, img_width, img_height, patch_size):
             k += 1
     return upscaled_img
 
-if __name__ == "__main__":
+def superres_upscaler_gradio(input_image, steps):
+    steps = int(steps)
+
+    model_id = "stabilityai/stable-diffusion-x4-upscaler"
+    pipeline = StableDiffusionUpscalePipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    pipeline = pipeline.to("cuda")
+
+    low_res_img = input_image.convert("RGB")
+    original_width, original_height = low_res_img.size
+
+    closest_width = int(round(original_width / 128)) * 128 ###
+    closest_height = int(round(original_height / 128)) * 128 ###
+    low_res_img = low_res_img.resize((closest_width, closest_height))###
+    new_size = (closest_width, closest_height) ###
+    
+    patch_size = 128
+    patches = divide_image(low_res_img, patch_size)
+
+    prompt = "enhance detail"
+    upscaled_patches = []
+    for patch in patches:
+        upscaled_patch = pipeline(prompt=prompt, image=patch, num_inference_steps=steps).images[0]
+        upscaled_patches.append(upscaled_patch)
+
+    upscaled_image = reassemble_image(upscaled_patches, new_size[0], new_size[1], patch_size)
+    upscaled_image = upscaled_image.resize((original_width * 4, original_height * 4))
+
+    os.makedirs("outputs/gradio/superres", exist_ok=True)
+    upscaled_image.save("outputs/gradio/superres/superres_upscaler_output_gradio.png")
+    return upscaled_image
+
+    if __name__ == "__main__":
 
     model_id = "stabilityai/stable-diffusion-x4-upscaler"
     pipeline = StableDiffusionUpscalePipeline.from_pretrained(model_id, torch_dtype=torch.float16)
@@ -62,48 +93,3 @@ if __name__ == "__main__":
 
     os.makedirs("outputs/superres", exist_ok=True)
     upscaled_image.save("outputs/superres/superres_upscaler_output.png")
-
-def superres_upscaler_gradio(input_image, steps):
-    steps = int(steps)
-
-    model_id = "stabilityai/stable-diffusion-x4-upscaler"
-    pipeline = StableDiffusionUpscalePipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-    pipeline = pipeline.to("cuda")
-
-    low_res_img = input_image.convert("RGB")
-    original_width, original_height = low_res_img.size
-
-    closest_width = int(round(original_width / 128)) * 128 ###
-    closest_height = int(round(original_height / 128)) * 128 ###
-    low_res_img = low_res_img.resize((closest_width, closest_height))###
-    new_size = (closest_width, closest_height) ###
-
-    # if original_width < 350 or original_height < 350:
-    #     low_res_img = low_res_img.resize((256, 256))
-    #     new_size = (256, 256)
-    # else:
-    #     if original_width > original_height:
-    #         low_res_img = low_res_img.resize((768, 512))
-    #         new_size = (768, 512)
-    #     elif original_width < original_height:
-    #         low_res_img = low_res_img.resize((512, 768))
-    #         new_size = (512, 768)
-    #     else:
-    #         low_res_img = low_res_img.resize((512, 512))
-    #         new_size = (512, 512)
-    
-    patch_size = 128
-    patches = divide_image(low_res_img, patch_size)
-
-    prompt = "enhance detail"
-    upscaled_patches = []
-    for patch in patches:
-        upscaled_patch = pipeline(prompt=prompt, image=patch, num_inference_steps=steps).images[0]
-        upscaled_patches.append(upscaled_patch)
-
-    upscaled_image = reassemble_image(upscaled_patches, new_size[0], new_size[1], patch_size)
-    upscaled_image = upscaled_image.resize((original_width * 4, original_height * 4))
-
-    os.makedirs("outputs/gradio/superres", exist_ok=True)
-    upscaled_image.save("outputs/gradio/superres/superres_upscaler_output_gradio.png")
-    return upscaled_image

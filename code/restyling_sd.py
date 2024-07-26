9564b0d4bc7cd55698ba391ffc822dd3b88a92a5
Matheus Ribeiro de Oliveira <s0073@atcremers65.in.tum.de>
2024-07-26 15:30:39 +0200

    minor changes

--- code/restyling_sd.py
+++ code/restyling_sd.py
@@ -5,6 +5,35 @@ from io import BytesIO
 
 from diffusers import StableDiffusionImg2ImgPipeline
 
+def restyling_gradio(input_image, text_prompt, strength, guidance_scale, negative_prompt, steps):
+    device = "cuda"
+    model_id_or_path = "runwayml/stable-diffusion-v1-5"
+    pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id_or_path, torch_dtype=torch.float16)
+    pipe = pipe.to(device)
+
+    strength = float(strength)
+    guidance_scale = float(guidance_scale)
+    steps = int(steps)
+    input_image = input_image.convert("RGB")
+    original_width, original_height = input_image.size
+
+    if original_width > original_height:
+        input_image = input_image.resize((768, 512))
+    elif original_width < original_height:
+        input_image = input_image.resize((512, 768))
+    else:
+        input_image = input_image.resize((512, 512))
+    
+    images = pipe(prompt=text_prompt, image=input_image, strength=strength, guidance_scale=guidance_scale, negative_prompt = negative_prompt, num_inference_steps=steps).images
+
+    output_image = images[0].resize((original_width, original_height))
+
+    output_dir = "outputs/gradio"
+    filename = "restyling_output_sdv15_new.png"
+    output_image.save(f"{output_dir}/{filename}")
+
+    return output_image
+
 if __name__ == "__main__":
     device = "cuda"
     model_id_or_path = "runwayml/stable-diffusion-v1-5"
@@ -37,32 +66,3 @@ if __name__ == "__main__":
     output_dir = "outputs/restyling"
     filename = "restyling_output.png"
     output_image.save(f"{output_dir}/{filename}")
-
-def restyling_gradio(input_image, text_prompt, strength, guidance_scale, negative_prompt, steps):
-    device = "cuda"
-    model_id_or_path = "runwayml/stable-diffusion-v1-5"
-    pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id_or_path, torch_dtype=torch.float16)
-    pipe = pipe.to(device)
-
-    strength = float(strength)
-    guidance_scale = float(guidance_scale)
-    steps = int(steps)
-    input_image = input_image.convert("RGB")
-    original_width, original_height = input_image.size
-
-    if original_width > original_height:
-        input_image = input_image.resize((768, 512))
-    elif original_width < original_height:
-        input_image = input_image.resize((512, 768))
-    else:
-        input_image = input_image.resize((512, 512))
-    
-    images = pipe(prompt=text_prompt, image=input_image, strength=strength, guidance_scale=guidance_scale, negative_prompt = negative_prompt, num_inference_steps=steps).images
-
-    output_image = images[0].resize((original_width, original_height))
-
-    output_dir = "outputs/gradio"
-    filename = "restyling_output_sdv15_new.png"
-    output_image.save(f"{output_dir}/{filename}")
-
-    return output_image
